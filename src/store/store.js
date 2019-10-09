0c012b2498c59c1bc53a031e1673b0ef7cc740b7
Anthony Clifton <anthony.s.clifton@accenture.com>
2019-10-09 14:34:09 -0500

    Added unconnected empty store

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { createStore } from "redux";
+import combinedReducers from "../reducers/index";
+
+function prepareStore(state = {}) {
+    return createStore(combinedReducers,state);
+}
+
+export default prepareStore;
