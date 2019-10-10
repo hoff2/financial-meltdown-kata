f47abf79dc27f045f9fa6b8901294db788dbeaf2
Anthony Clifton <anthony.s.clifton@accenture.com>
2019-10-10 09:40:43 -0500

    Added enzyme and the application container

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import { createStore } from "redux";
 import combinedReducers from "../reducers/index";
 
 function prepareStore(state = {}) {
-    return createStore(combinedReducers,state);
+    return createStore(combinedReducers, state);
 }
 
 export default prepareStore;
---
+++ test/views/credit-application/credit-application-container.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import CreditApplicationContainer from "../../../src/views/credit-application/credit-application-container";
+
+describe("CreditApplicationContainer", () => {
+    test("should contain header", () => {
+        const container = shallow(<CreditApplicationContainer/>);
+        const header = container.find('header');
+
+        expect(header.length).toEqual(1);
+    });
+});
