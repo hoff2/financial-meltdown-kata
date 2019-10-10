cdeea5666349481c93e4d596a212a8d6d09f1882
Anthony Clifton <anthony.s.clifton@accenture.com>
2019-10-10 09:54:38 -0500

    Added empty redux connector

---
+++ src/views/credit-application/credit-application-connector.js
@@ -0,0 +1,8 @@
+import {connect} from 'react-redux';
+import CreditApplicationContainer from "./credit-application-container";
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = state => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreditApplicationContainer);
---
+++ test/views/credit-application/credit-application-connector.spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CreditApplicationConnector from '../../../src/views/credit-application/credit-application-connector';
+import {Provider} from "react-redux";
+import prepareStore from "../../../src/store/store";
+
+describe("CreditApplicationConnector", () => {
+    test("should render a CreditApplicationContainer", () => {
+        const div = document.createElement('div');
+        ReactDOM.render(
+            <Provider store={prepareStore()}>
+                <CreditApplicationConnector/>
+            </Provider>, div
+        );
+    });
+});
