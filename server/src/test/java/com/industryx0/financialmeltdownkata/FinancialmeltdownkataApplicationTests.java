6db5e8b08639bd79fae93165067438d19765c86a
Anthony Clifton <anthony.s.clifton@accenture.com>
2019-10-15 09:40:57 -0500

    Added base spring boot app with only health endpoints

---
+++ server/src/test/java/com/industryx0/financialmeltdownkata/FinancialmeltdownkataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.industryx0.financialmeltdownkata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinancialmeltdownkataApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
