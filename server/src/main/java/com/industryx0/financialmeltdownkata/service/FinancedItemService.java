d0d6f29b5c99d25a8b8576d839b0369968d6a89e
Anthony Clifton <anthony.s.clifton@accenture.com>
2019-10-23 10:49:51 -0500

    Named constants for financial calculations as upper snake case

--- server/src/main/java/com/industryx0/financialmeltdownkata/service/FinancedItemService.java
+++ server/src/main/java/com/industryx0/financialmeltdownkata/service/FinancedItemService.java
@@ -13,8 +13,8 @@ import java.util.UUID;
 public class FinancedItemService {
     private HashMap<String, FinancedItem> financedItems = new HashMap<>();
 
-    final private BigDecimal paymentTerm = new BigDecimal(12);
-    final private BigDecimal interestDivisor = new BigDecimal(10);
+    final private BigDecimal PAYMENT_TERM = new BigDecimal(12);
+    final private BigDecimal INTEREST_DIVISOR = new BigDecimal(10);
 
     public FinancedItem postFinancedItem(FinancedItem financedItem) {
         if (financedItem.getId() == null || !financedItems.containsKey(financedItem.getId())) {
@@ -22,8 +22,8 @@ public class FinancedItemService {
             financedItem.setId(id);
         }
 
-        financedItem.setMinimumPayment(financedItem.getPrice().divide(this.paymentTerm, RoundingMode.HALF_DOWN));
-        financedItem.setRate(financedItem.getPrice().divide(this.interestDivisor, RoundingMode.HALF_DOWN));
+        financedItem.setMinimumPayment(financedItem.getPrice().divide(this.PAYMENT_TERM, RoundingMode.HALF_DOWN));
+        financedItem.setRate(financedItem.getPrice().divide(this.INTEREST_DIVISOR, RoundingMode.HALF_DOWN));
 
         financedItems.put(financedItem.getId(), financedItem);
         return financedItem;
